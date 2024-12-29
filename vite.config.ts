8fbf4924e79654c4b789c59b73c2d1c9503fa692
Shubhangi Raj <shubhangi120989@gmail.com>
2024-12-29 13:07:49 +0530

    changed vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': {}
+//   }
+// })
+
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   define: {
-    'process.env': {}
-  }
-})
+        'process.env': {}
+      },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Ensure no unnecessary chunks are created
+      },
+    },
+  },
+});
